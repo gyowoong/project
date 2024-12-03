2f3042a7b237806c54b9b029f6ea3b1b951eef8e
이휘주 <gnlwn32122@gmail.com>
2024-12-03 15:36:15 +0900

    test

--- src/main/java/com/example/project/service/MemberService.java
+++ src/main/java/com/example/project/service/MemberService.java
@@ -2,6 +2,7 @@ package com.example.project.service;
 
 public class MemberService {
     private int bno;
+    private String test;
     private Long rno;
     private String name;
     private String email4;
