42db886b2c25af9f25fe76a260c52a50485d79d3
ju <roronoa246@gmail.com>
2024-12-07 20:33:09 +0900

    프로젝트 영화 템플릿 디자인 수정

--- src/main/java/com/example/project/config/SecurityConfig.java
+++ src/main/java/com/example/project/config/SecurityConfig.java
@@ -7,14 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-<<<<<<< HEAD
-@EnableMethodSecurity
-@EnableWebSecurity
-@Configuration
-public class SecurityConfig {
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-=======
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
@@ -22,13 +14,8 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
->>>>>>> origin/main
         http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
 
         return http.build();
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
 }
--- src/main/java/com/example/project/controller/HomeController.java
+++ src/main/java/com/example/project/controller/HomeController.java
@@ -1,37 +1,18 @@
 package com.example.project.controller;
 
-<<<<<<< HEAD
-import org.springframework.stereotype.Controller;
-
-import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-=======
 import lombok.extern.log4j.Log4j2;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
->>>>>>> origin/main
 
 @Log4j2
 @Controller
 public class HomeController {
-<<<<<<< HEAD
-    
-    @GetMapping("/")
-    public String getMethodName() {
-        return "redirect:/index";
-    }
-    
-=======
 
     @GetMapping("/")
-    public String getHome() {
+    public String getIndex() {
         log.info("home 폼 요청");
-        return "/index";
+        return "/movie/main";
     }
 
->>>>>>> origin/main
 }
