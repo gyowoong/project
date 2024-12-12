3c288471e03b628cead7dfe99b544d80e9e10250
leejoonwoo <leejoonwoo.kr@gmail.com>
2024-12-12 18:15:29 +0900

    검색1

--- src/main/java/com/example/project/controller/MovieController.java
+++ src/main/java/com/example/project/controller/MovieController.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Log4j2
 @Controller
@@ -33,19 +35,24 @@ public class MovieController {
     }
 
     @GetMapping("/movieList")
-    public void getMovieList(Long genre, Long page, String movieList, Model model) {
+    public void getMovieList(Long genre, Long page, String movieList, String type, String keyword, Model model) {
         log.info("movieList 폼 요청 {}", page);
         model.addAttribute("movieList", movieList);
         model.addAttribute("genre", genre);
+        model.addAttribute("type", type);
+        model.addAttribute("keyword", keyword);
         model.addAttribute("page", page);
     }
 
     @GetMapping("/movieDetail")
-    public void getMovieDetail(Long id, String movieList, Long genre, Long page, Model model) {
+    public void getMovieDetail(Long id, String movieList, Long genre, String type, String keyword, Long page,
+            Model model) {
         log.info("movieDetail 폼 요청 {}", id);
         model.addAttribute("id", id);
         model.addAttribute("movieList", movieList);
         model.addAttribute("genre", genre);
+        model.addAttribute("type", type);
+        model.addAttribute("keyword", keyword);
         model.addAttribute("page", page);
     }
 
