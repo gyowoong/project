8c1417e5abc8e7f005073d76d23542e5d0c7fb71
leejoonwoo <leejoonwoo.kr@gmail.com>
2024-12-16 00:26:26 +0900

    영화 정보 출력 테스트

--- src/main/java/com/example/project/repository/MovieRepository.java
+++ src/main/java/com/example/project/repository/MovieRepository.java
@@ -4,15 +4,48 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import com.example.project.entity.Movie;
+import com.example.project.entity.QMovie;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+public interface MovieRepository extends JpaRepository<Movie, Long>, QuerydslPredicateExecutor<Movie> {
 
     @Query("SELECT g.name FROM Movie m JOIN MovieGenre mg ON m.id = mg.movie.id JOIN Genre g ON mg.genre.id = g.id WHERE m.id = :id")
     List<String> getGenre(Long id);
 
     @Query("SELECT p.name FROM Movie m JOIN MoviePeople mp ON m.id = mp.movie.id JOIN People p ON mp.people.id = p.id WHERE m.id = :id")
     List<String> getpeople(Long id);
+
+    default Predicate makePredicate(String type, String keyword) {
+        BooleanBuilder builder = new BooleanBuilder();
+        QMovie qMovie = QMovie.movie;
+
+        // id > 0: range scan
+        builder.and(qMovie.id.gt(0L));
+
+        if (type == null)
+            return builder;
+
+        // // 카테고리
+        // if (type.equals("c")) {
+        // builder.and(qMovie.category.name.contains(keyword));
+        // }
+        // // 제목
+        // else if (type.equals("t")) {
+        // builder.and(qMovie.title.contains(keyword));
+        // }
+        // // 저자
+        // else if (type.equals("w")) {
+        // builder.and(qMovie.writer.contains(keyword));
+        // }
+        if (type.equals("t")) {
+            builder.and(qMovie.title.contains(keyword));
+        }
+
+        return builder;
+    }
 
 }
---
+++ src/main/java/com/example/project/service/MovieService.java
@@ -0,0 +1,60 @@
+package com.example.project.service;
+
+import java.util.List;
+
+import com.example.project.dto.MovieDto;
+import com.example.project.dto.PageRequestDTO;
+import com.example.project.dto.PageResultDTO;
+import com.example.project.entity.Movie;
+
+public interface MovieService {
+
+    PageResultDTO<MovieDto, Movie> getList(PageRequestDTO requestDto);
+
+    List<String> getGenre(Long id);
+
+    public default MovieDto entityToDto(Movie movie) {
+        return MovieDto.builder()
+                .id(movie.getId())
+                .backdrop_path(movie.getBackdrop_path())
+                .budget(movie.getBudget())
+                .homepage(movie.getHomepage())
+                .originalLanguage(movie.getOriginalLanguage())
+                .originalTitle(movie.getOriginalTitle())
+                .overview(movie.getOverview())
+                .popularity(movie.getPopularity())
+                .posterPath(movie.getPosterPath())
+                .releaseDate(movie.getReleaseDate())
+                .revenue(movie.getRevenue())
+                .runtime(movie.getRuntime())
+                .status(movie.getStatus())
+                .tagline(movie.getTagline())
+                .title(movie.getTitle())
+                .voteAverage(movie.getVoteAverage())
+                .voteCount(movie.getVoteCount())
+                .build();
+    }
+
+    public default Movie dtoToEntity(MovieDto movieDto) {
+        return Movie.builder()
+                .id(movieDto.getId())
+                .backdrop_path(movieDto.getBackdrop_path())
+                .budget(movieDto.getBudget())
+                .homepage(movieDto.getHomepage())
+                .originalLanguage(movieDto.getOriginalLanguage())
+                .originalTitle(movieDto.getOriginalTitle())
+                .overview(movieDto.getOverview())
+                .popularity(movieDto.getPopularity())
+                .posterPath(movieDto.getPosterPath())
+                .releaseDate(movieDto.getReleaseDate())
+                .revenue(movieDto.getRevenue())
+                .runtime(movieDto.getRuntime())
+                .status(movieDto.getStatus())
+                .tagline(movieDto.getTagline())
+                .title(movieDto.getTitle())
+                .voteAverage(movieDto.getVoteAverage())
+                .voteCount(movieDto.getVoteCount())
+                .build();
+    }
+
+}
