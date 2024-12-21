28e0574450eac59270156e8996feec5746458910
ju <roronoa246@gmail.com>
2024-12-21 18:18:42 +0900

    테이블 충돌

--- src/main/java/com/example/project/repository/reserve/ReserveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repository.reserve;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.project.entity.Reserve;
-
-public interface ReserveRepository extends JpaRepository<Reserve, Long> {
-
-}
--- src/main/java/com/example/project/repository/reserve/TheaterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repository.reserve;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.project.entity.Theater;
-
-public interface TheaterRepository extends JpaRepository<Theater, Long> {
-
-}
