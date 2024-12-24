7fbdeeef2b2c5d02d0d21721632ff4b34b99b0cd
이교웅 <landjjang1212@gmail.com>
2024-12-24 17:47:20 +0900

    이메일 문의 및 상담내역 수정 ing

--- src/main/java/com/example/project/controller/CounselingController.java
+++ src/main/java/com/example/project/controller/CounselingController.java
@@ -13,13 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.project.entity.Counseling;
-<<<<<<< HEAD
 import com.example.project.entity.Inquiry;
 import com.example.project.repository.CounselingRepository;
 import com.example.project.repository.InquiryRepository;
-=======
-import com.example.project.repository.CounselingRepository;
->>>>>>> 560831b48a02e4345a2370324d58d0c8cdf674ab
 
 @Controller
 public class CounselingController {
@@ -27,7 +23,6 @@ public class CounselingController {
     @Autowired
     private CounselingRepository counselingRepository;
 
-<<<<<<< HEAD
     @Autowired
     private InquiryRepository inquiryRepository;
 
@@ -63,28 +58,4 @@ public class CounselingController {
         return "center/counselingPage";
     }
 
-=======
-    @GetMapping("/center/counseling")
-    public String getCounselingHistory(@RequestParam(defaultValue = "1") int page, Model model) {
-
-        // 이메일 문의 내역 조회
-        List<Counseling> counselings = counselingRepository.findAll(); // 상담 내역 조회
-
-        // 예시: 페이지당 항목 수 (예: 10개)
-        int pageSize = 10;
-        int totalCounselings = counselings.size(); // 전체 상담 내역의 개수
-        int totalPages = (int) Math.ceil((double) totalCounselings / pageSize); // 총 페이지 수 계산
-
-        // 상담 내역 페이징 처리
-        Pageable pageable = PageRequest.of(page - 1, pageSize);
-        Page<Counseling> counselingsPage = counselingRepository.findAll(pageable);
-
-        // 이메일 문의 내역을 포함하여 상담 내역 추가
-        model.addAttribute("counselings", counselingsPage.getContent());
-        model.addAttribute("totalPages", totalPages);
-        model.addAttribute("cuurentPage", page);
-
-        return "/center/counseling"; // counseling.html로 상담 내역을 전달
-    }
->>>>>>> 560831b48a02e4345a2370324d58d0c8cdf674ab
 }
